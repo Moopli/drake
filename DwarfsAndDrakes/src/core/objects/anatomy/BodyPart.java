5a7968839bc57097c8dbd337f53e1ec1614ba69d
Filip Burlacu <Moopli@users.noreply.github.com>
2013-01-06 20:24:29 -0500

    some more item work

--- DwarfsAndDrakes/src/core/objects/anatomy/BodyPart.java
+++ DwarfsAndDrakes/src/core/objects/anatomy/BodyPart.java
@@ -16,7 +16,7 @@ import java.util.*;
  * 
  * @author filip
  */
-public class BodyPart extends DamageableArea {
+public class BodyPart {
     
     
     public String internalName;
@@ -24,6 +24,7 @@ public class BodyPart extends DamageableArea {
     public HashSet<BodyPart> connected = new HashSet<BodyPart>();
     public HashSet<String> functionsProvided = new HashSet<String>();
     public HashSet<String> functionsRequired = new HashSet<String>();
+    public HashSet<String> equipTags = new HashSet<String>();
     
     // if an organ is vaporized, it hardly has nerves or blood vessels, does it?
     boolean supportsInfrastructure = true; 
@@ -31,4 +32,47 @@ public class BodyPart extends DamageableArea {
     // a body part may be wholy within another
     BodyPart contained_inside;
     
+    // ## MERGED FROM DAMAGEABLEAREA
+    
+    ArrayList<BodyPart> contained_parts = new ArrayList<BodyPart>();
+    
+    boolean isArmorable = false; // you can't wear a spleenplate, but you can sure wear a guantlet!
+    
+    int volume; // overall volume, determines likelihood to be hit (along with length) and penetration depth if hit (along with hardness)
+    int length; // length of major axis, to determine how likely it is to be hit
+    int hardness; // how easy it is to cut/poke/smash through?
+    
+    
+    /*
+     * Yup, here it is; the big "damage that thing" method.
+     * 
+     * Things that can happen when a damageable area gets hit -- 
+     * - surface damage (bruising, cuts)
+     *  - based on this.hardness and collision parameters, we can tell if the 
+     *    resulting penetration depth is low enough to ... not penetrate.
+     * - penetration, misses stuff inside (oh thank goodness! that arrow managed 
+     *   to miss every internal organ in my gut!)
+     *  - If we rule penetration, based on relative volumes and stuff we can 
+     *    decide that either soemthing got hit, or nothing got hit. Bad either 
+     *    way.
+     * - penetration, hits something inside (aah my spleen!)
+     *  - Likely causes loss of function
+     * 
+     */
+    
+    public void damage(int contact_area, int contact_impulse){
+        
+    }
+    
+    // properties storing just howw damaged it is
+    
+    /*
+     * mayhaps we could use a current HP vs. minimum HP to function system --
+     * we change descriptors based on where current HP is, and say things like 
+     * "about to fail", or "perfectly healthy".
+     * What's more, the minHP value could be moved around, and so could curHP, 
+     * based on something like a constitution number.
+     */
+    
+    int curHP, minHP;
 }
--- DwarfsAndDrakes/src/core/objects/equips/Inventory.java
+++ DwarfsAndDrakes/src/core/objects/equips/Inventory.java
@@ -15,7 +15,6 @@ public class Inventory {
     
     
     /*
-     * Not sure if using an entry is good idea, but we'll figure out
      * (note on the mapping: item name -> list of same item (a stack)
      */
     public HashMap<String, ArrayList<InventoryItem>> inventory;
