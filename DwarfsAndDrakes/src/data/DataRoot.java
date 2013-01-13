c987524f1d8725aedd901e81645f48a277283837
filip <filip@ubuntu>
2013-01-12 22:49:06 -0500

    Data restructuring and object handling

---
+++ DwarfsAndDrakes/src/data/DataRoot.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+/**
+ * This is a stub class; used as the placeholder for the root of the data file 
+ * directories.
+ * @author filip
+ */
+public final class DataRoot {
+    
+    private DataRoot(){}
+}
