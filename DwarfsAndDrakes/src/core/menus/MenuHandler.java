20cf304fea8c95a02ab1e995b124da70143d1c37
filip <filip@ubuntu>
2013-01-19 22:30:01 -0500

    interfacing

---
+++ DwarfsAndDrakes/src/core/menus/MenuHandler.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core.menus;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author filip
+ */
+public class MenuHandler {
+    
+    HashMap<String, Menu> menuMap = new HashMap<String, Menu>();
+    
+    
+    
+    
+}
