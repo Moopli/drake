509926a600043bcfb917459cb972f150a4785b09
Filip Burlacu <Moopli@users.noreply.github.com>
2013-01-20 21:57:47 -0500

    Map generation extended to spawn goblinches, player, and stairs in random places

--- DwarfsAndDrakes/src/core/objects/map/generation/MapGenerator.java
+++ DwarfsAndDrakes/src/core/objects/map/generation/MapGenerator.java
@@ -14,10 +14,45 @@ public class MapGenerator {
     
     public static final String PATH_PREFIX = "maps/";
     
-    public static void generateMap(int w, int h, String path) {
+    public static void generateMap(int w, int h, int goblinses, String path) {
         BSPRect dungeon = new BSPRect(0, 0, w, h);
         dungeon.partition(0.6);
         dungeon.joinRooms();
+        
+        int gobCount = 0;
+        while (gobCount < goblinses){
+            int x = (int)(Math.random() * dungeon.room.get(0).size());
+            int y = (int)(Math.random() * dungeon.room.size());
+            
+            if (dungeon.room.get(y).get(x).equals(".")){
+                dungeon.room.get(y).set(x, "g");
+                gobCount++;
+            }
+        }
+        
+        boolean droppedPlayer = false;
+        while (!droppedPlayer){
+            int x = (int)(Math.random() * dungeon.room.get(0).size());
+            int y = (int)(Math.random() * dungeon.room.size());
+            
+            if (dungeon.room.get(y).get(x).equals(".")){
+                dungeon.room.get(y).set(x, "@");
+                droppedPlayer = true;
+            }
+        }
+        
+        
+        int stairCount = 0;
+        while (stairCount < 3){
+            int x = (int)(Math.random() * dungeon.room.get(0).size());
+            int y = (int)(Math.random() * dungeon.room.size());
+            
+            if (dungeon.room.get(y).get(x).equals(".")){
+                dungeon.room.get(y).set(x, "<");
+                stairCount++;
+            }
+        }
+        
         try {
             FileWriter fstream = new FileWriter(path);
             BufferedWriter out = new BufferedWriter(fstream);
@@ -30,6 +65,6 @@ public class MapGenerator {
     }
     
     public static void main(String[] args) {
-        generateMap(40, 40, "test.map");
+        generateMap(40, 40, 12, "test.map");
     }
 }
